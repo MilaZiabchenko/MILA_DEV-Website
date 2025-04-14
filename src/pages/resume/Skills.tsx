ccd70c01dc41e89124432479133075a3b54e780e
Mila Ziabchenko <mi.podgurska@gmail.com>
2025-04-14 09:15:43 +0300

    Update Skills.tsx

--- src/pages/resume/Skills.tsx
+++ src/pages/resume/Skills.tsx
@@ -3,10 +3,10 @@ const Skills = () => (
     <h3>Skills</h3>
     <ul>
       <li>
-        <span>Programming Languages:</span> JavaScript, TypeScript, Rust
+        <span>Programming Languages:</span> JavaScript (ES6+), TypeScript, Rust
       </li>
       <li>
-        <span>Front End:</span> HTML, CSS/SCSS, React, Redux Toolkit, Next, Styled Components
+        <span>Front End:</span> HTML, CSS/SCSS, React/Next, Redux Toolkit, Tailwind, Chakra UI
       </li>
       <li>
         <span>Back End:</span> Node, Express, MongoDB, Firebase
