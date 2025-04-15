cf8dce9688d5e7c9bc89b1938a75db38d2c81a84
Mila Ziabchenko <mi.podgurska@gmail.com>
2025-04-15 21:59:01 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -21,8 +21,8 @@ const About = () => (
       I love to design and build amazing apps with great functionality and
       beautiful interfaces for everyone, taking advantage of the cutting-edge
       technologies and best practices. I am a constant learner who creates apps
-      with attention to every detail and with a big picture in mind. I always
-      try to deliver the best user experience possible.
+      with attention to every detail without losing sight of the big picture. I
+      always try to deliver the best user experience possible.
     </p>
     <p>
       In <Link to='/articles'>my articles</Link> written for this blog dedicated
@@ -31,11 +31,14 @@ const About = () => (
       friendly feedback 😉
     </p>
     <p>
-      I have also worked as an{' '}
+      I'm also an{' '}
       <Link to='/resume/interpreter'>
         interpreter of Italian, Spanish and English languages
       </Link>
-      , primarily in the realm of international adoptions in Ukraine, helping
+      , who ficilitates communication between people that speak different languages.
+    </p>
+    <p>
+      In the past, I worked in the realm of international adoptions, helping
       Ukrainian orphan children to join loving families from foreign countries.
     </p>
     <p>
@@ -46,7 +49,8 @@ const About = () => (
       My other interests include (but are not limited to) spending time with my
       loved ones; communicating with kind and interesting people; walking,
       hiking, biking, swimming, traveling; learning new things, especially
-      related to cognitive, social and other sciences.
+      related to cognitive and social sciences, international relations and 
+      geopolitics.
     </p>
     <p>
       I adore drinking latte ☕ and eating delicious food, especially fruit
